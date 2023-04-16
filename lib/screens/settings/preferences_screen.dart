76166b3a6506e56ef3b3b4c98e6ebd63148fdff4
Winner13KS <kuandyksanzar020@gmail.com>
2023-04-16 12:17:38 +0600

    remaked preferences screen

--- lib/screens/settings/preferences_screen.dart
+++ lib/screens/settings/preferences_screen.dart
@@ -13,83 +13,71 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
   Color purpleBeaty = const Color(0xFF8E05C2);
 
   @override
+  void initState() {
+    categories.sort();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 70.0),
+          padding: const EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-                child: Text(
-                  'Please select preferences',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                    fontSize: 24,
-                    color: littleLight,
-                  ),
+              Text(
+                'Please select preferences',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 24,
+                  color: littleLight,
                 ),
               ),
               const SizedBox(height: 24.0),
-              Column(
-                children:
-                    List.generate((categories.length / 3).ceil(), (index) {
-                  int startIndex = index * 3;
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate(3, (rowIndex) {
-                      int categoryIndex = startIndex + rowIndex;
-                      if (categoryIndex >= categories.length) {
-                        return const SizedBox();
-                      }
-                      return Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            print("tapped");
-                          },
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 150,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                      color: Colors.black12, width: 1),
-                                ),
-                                child: Align(
-                                  alignment: Alignment.topCenter,
-                                  child: Image.asset(
-                                    categories.values.toList()[categoryIndex],
-                                    fit: BoxFit.cover,
-                                    height: 150,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 50,
-                                child: Center(
-                                  child: Text(
-                                    categories.keys.toList()[categoryIndex],
-                                    style: TextStyle(
-                                      fontFamily: 'WorkSans',
-                                      fontSize: 15,
-                                      color: littleLight,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: List.generate(categories.length, (index) {
+                      return GestureDetector(
+                        onTap: () {
+                          print(categories[index]);
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                            vertical: 16.0,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                blurRadius: 5.0,
+                                offset: const Offset(0, 2),
                               ),
                             ],
+                            color: Colors.grey.shade900,
+                          ),
+                          child: Text(
+                            categories[index],
+                            style: const TextStyle(
+                              fontFamily: 'WorkSans',
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.start,
                           ),
                         ),
                       );
                     }),
-                  );
-                }),
+                  ),
+                ),
               ),
             ],
           ),
