567e9feb532f3d89a6025751036cc454fce1f5ea
Winner13KS <kuandyksanzar020@gmail.com>
2023-05-22 12:44:11 +0600

    basic load on scroll

--- lib/screens/main/tab_bar_screens/recommendation_screen.dart
+++ lib/screens/main/tab_bar_screens/recommendation_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 import '../../../api/recommendations_api.dart';
 import '../../../models/channel_model.dart';
 import '../../../models/post_model.dart';
 
 class RecommendationScreen extends StatefulWidget {
-  const RecommendationScreen({super.key});
+  const RecommendationScreen({Key? key}) : super(key: key);
 
   @override
   State<RecommendationScreen> createState() => _RecommendationScreenState();
@@ -17,42 +16,64 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
 
   String currentPressedFilter = 'All';
   List<String> filterOptions = ['All', 'Trending'];
-  // ignore: prefer_final_fields
   List<bool> _isSelected = [true, false, false, false];
+
+  ScrollController _scrollController = ScrollController();
+  Map<Channel, Post>? recommendations;
+  int pageNumber = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+    initRecommendations();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   void _onFilterButtonPressed(int index) {
     setState(() {
-      // Reset all the buttons to unselected
       for (int buttonIndex = 0;
           buttonIndex < _isSelected.length;
           buttonIndex++) {
         _isSelected[buttonIndex] = false;
       }
-
-      // Select the pressed button
       _isSelected[index] = true;
-
-      // Update the current pressed filter
       currentPressedFilter = filterOptions[index];
     });
   }
 
-  Map<Channel, Post>? recommendations;
-
-  @override
-  initState() {
-    super.initState();
-    // Fetch recommendations when the page is entered
-    initRecommendations();
+  Future<void> initRecommendations() async {
+    final response = await fetchRecommendations(pageNumber);
+    setState(() {
+      recommendations = response;
+    });
   }
 
-  Future<void> initRecommendations() async {
-    final response = await fetchRecommendations(0);
-    recommendations = response;
+  Future<void> loadMoreRecommendations() async {
+    pageNumber++; // Increment the page number for fetching new content
+
+    final response = await fetchRecommendations(pageNumber);
+    setState(() {
+      recommendations?.addAll(response);
+    });
+  }
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      loadMoreRecommendations();
+    }
   }
 
   Future<void> refreshRecommendations() async {
-    final response = await fetchRecommendations(0);
+    pageNumber = 0; // Reset the page number when refreshing
+    final response = await fetchRecommendations(pageNumber);
     setState(() {
       recommendations = response;
     });
@@ -63,32 +84,34 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
     List<Widget> filterButtons = [];
 
     for (int i = 0; i < filterOptions.length; i++) {
-      filterButtons.add(Padding(
-        padding: const EdgeInsets.only(right: 10.0),
-        child: ElevatedButton(
-          onPressed: () => _onFilterButtonPressed(i),
-          style: ElevatedButton.styleFrom(
-            fixedSize: const Size(100, 35),
-            backgroundColor: _isSelected[i] ? littleLight : Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(
-                color: _isSelected[i] ? littleLight : purpleBeaty,
-                width: 1,
+      filterButtons.add(
+        Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: ElevatedButton(
+            onPressed: () => _onFilterButtonPressed(i),
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(100, 35),
+              backgroundColor: _isSelected[i] ? littleLight : Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  color: _isSelected[i] ? littleLight : purpleBeaty,
+                  width: 1,
+                ),
+              ),
+            ),
+            child: Text(
+              filterOptions[i],
+              style: TextStyle(
+                fontFamily: 'WorkSans',
+                fontWeight: FontWeight.w500,
+                fontSize: 13,
+                color: _isSelected[i] ? Colors.black : Colors.white,
               ),
             ),
           ),
-          child: Text(
-            filterOptions[i],
-            style: TextStyle(
-              fontFamily: 'WorkSans',
-              fontWeight: FontWeight.w500,
-              fontSize: 13,
-              color: _isSelected[i] ? Colors.black : Colors.white,
-            ),
-          ),
         ),
-      ));
+      );
     }
 
     return Scaffold(
@@ -114,9 +137,6 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
                 child: RefreshIndicator(
                   onRefresh: refreshRecommendations,
                   child: Container(
-                    // margin: const EdgeInsets.only(
-                    //   top: 10,
-                    // ),
                     child: recommendations == null || recommendations!.isEmpty
                         ? Center(
                             child: Text(
@@ -129,6 +149,7 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
                             ),
                           )
                         : ListView.builder(
+                            controller: _scrollController,
                             itemCount: recommendations!.length,
                             itemBuilder: (BuildContext context, int index) {
                               final channels = recommendations!.keys.toList();
