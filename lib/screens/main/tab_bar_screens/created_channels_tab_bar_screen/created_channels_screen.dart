ec594631888092f1a31f90913d610322860f2262
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-09-14 13:20:42 +0600

    used channel widget

--- lib/screens/main/tab_bar_screens/created_channels_tab_bar_screen/created_channels_screen.dart
+++ lib/screens/main/tab_bar_screens/created_channels_tab_bar_screen/created_channels_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import '../../../../api/user_channels_api.dart';
-import '../../../../models/channel_model.dart';
+// import 'package:merume_mobile/colors.dart';
+
+import 'package:merume_mobile/models/channel_model.dart';
+import 'package:merume_mobile/api/user_channels_api.dart';
+import 'package:merume_mobile/screens/main/components/channel_widget.dart';
 
 class CreatedChannelsScreen extends StatefulWidget {
-  const CreatedChannelsScreen({super.key});
+  const CreatedChannelsScreen({Key? key}) : super(key: key);
 
   @override
   State<CreatedChannelsScreen> createState() => _CreatedChannelsScreenState();
@@ -21,13 +24,6 @@ class _CreatedChannelsScreenState extends State<CreatedChannelsScreen> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // SingleChildScrollView(
-              //   scrollDirection: Axis.horizontal,
-              //   child: Row(
-              //     mainAxisAlignment: MainAxisAlignment.start,
-              //     children: filterButtons,
-              //   ),
-              // ),
               const SizedBox(height: 16),
               Expanded(
                 child: StreamBuilder<List<Channel>>(
@@ -37,31 +33,8 @@ class _CreatedChannelsScreenState extends State<CreatedChannelsScreen> {
                       final channels = snapshot.data!;
                       return ListView.builder(
                         itemCount: channels.length,
-                        itemBuilder: (_, index) => Container(
-                          margin: const EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 5,
-                          ),
-                          padding: const EdgeInsets.all(20.0),
-                          decoration: BoxDecoration(
-                            color: const Color(0xff97FFFF),
-                            borderRadius: BorderRadius.circular(15.0),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                channels[index].name,
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Text(channels[index].description),
-                            ],
-                          ),
+                        itemBuilder: (_, index) => ChannelWidget(
+                          channel: channels[index],
                         ),
                       );
                     } else if (snapshot.hasError) {
