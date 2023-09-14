49d9b582958df004228eced523effb4dc700537b
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-09-14 13:20:02 +0600

    little fixes

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color mellowLemon = Color(0xFFF3FFAB);
-  static const Color royalPurple = Color(0xFF8E05C2);
-  static const Color lavenderHaze = Color(0xFFB7ABFF);
+  static const Color mellowLemon = Color(0xFFF3FFAB); // for text-blocks
+  static const Color royalPurple = Color(0xFF8E05C2); //for buttons-blocks
+  static const Color lavenderHaze = Color(0xFFB7ABFF); //for secondary
 }
