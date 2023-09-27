1e6b965ff8e55a3f1ee7746bb713d4b056c9c3c5
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-09-27 10:16:08 +0600

    basic post display

--- lib/screens/main/components/post_in_list_widget.dart
+++ lib/screens/main/components/post_in_list_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:merume_mobile/colors.dart';
-
 import 'package:merume_mobile/models/post_model.dart';
 
 class PostInListWidget extends StatelessWidget {
@@ -16,12 +15,13 @@ class PostInListWidget extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 12.5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Container(
             padding:
-                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
             decoration: BoxDecoration(
-              color: AppColors.postMain, // Change the background color here
+              color: AppColors.postMain,
               borderRadius: BorderRadius.circular(
                   10.0), // Adjust the corner radius as needed
             ),
@@ -30,6 +30,14 @@ class PostInListWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
+                  '@${post.ownerNickname}',
+                  style: const TextStyle(
+                    color: AppColors.mellowLemon,
+                    fontSize: 12,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
                   post.body,
                   style: const TextStyle(
                     color: Colors.white,
@@ -37,37 +45,27 @@ class PostInListWidget extends StatelessWidget {
                     fontSize: 16,
                   ),
                 ),
-                const SizedBox(height: 8.0), // Add spacing between lines
-                if (isError)
-                  Row(
-                    children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    if (isError)
                       const Icon(
-                        Icons.error_outline,
+                        Icons.error_outline_outlined,
                         color: Colors.red,
                         size: 20.0,
+                      )
+                    else
+                      const Icon(
+                        Icons.done,
+                        color: AppColors.lavenderHaze,
+                        size: 20.0,
                       ),
-                      const SizedBox(
-                          width: 4.0), // Add spacing between icon and text
-                      Text(
-                        'Author ID: ${post.ownerId} (Error)',
-                        style: const TextStyle(
-                          color: Colors.red,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                if (!isError)
-                  Text(
-                    '@${post.ownerNickname}',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                    ),
-                  ),
+                  ],
+                ),
               ],
             ),
-          )
+          ),
+          const SizedBox(width: 5.0),
         ],
       ),
     );
