c1c93b59f9cdab62fda42b6acc3646e0d2dc8291
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-10-15 12:18:15 +0600

    added datetime

--- lib/screens/main/components/post_in_list_widget.dart
+++ lib/screens/main/components/post_in_list_widget.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:merume_mobile/colors.dart';
 import 'package:merume_mobile/enums.dart';
+import 'package:intl/intl.dart';
 import 'package:merume_mobile/models/post_model.dart';
+import 'package:merume_mobile/screens/main/components/triangle.dart';
 
 class PostInListWidget extends StatelessWidget {
   final Post post;
   final MessageStatus status;
+  final bool isAuthor;
 
-  const PostInListWidget({Key? key, required this.post, required this.status})
+  const PostInListWidget(
+      {Key? key,
+      required this.post,
+      required this.status,
+      required this.isAuthor})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dateFormat = DateFormat("HH:mm");
+    final formattedDate = dateFormat.format(post.createdAt).toString();
+
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 12.5),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment:
+            isAuthor ? MainAxisAlignment.end : MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          if (!isAuthor) CustomPaint(painter: Triangle(Colors.grey[900]!)),
           Container(
             padding:
                 const EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
             decoration: BoxDecoration(
               color: AppColors.postMain,
-              borderRadius: BorderRadius.circular(10.0),
+              borderRadius: BorderRadius.circular(8.0),
             ),
             width: 300.0,
             child: Column(
@@ -47,7 +59,16 @@ class PostInListWidget extends StatelessWidget {
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+                    Text(
+                      formattedDate, // Display the formatted date
+                      style: const TextStyle(
+                        color: AppColors.lavenderHaze,
+                        fontSize: 13.0,
+                      ),
+                    ),
+                    const SizedBox(width: 8.0),
                     if (status == MessageStatus.error)
                       const Icon(
                         Icons.error_outline_outlined,
@@ -55,10 +76,14 @@ class PostInListWidget extends StatelessWidget {
                         size: 20.0,
                       )
                     else if (status == MessageStatus.waiting)
-                      const CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                            AppColors.lavenderHaze),
-                        strokeWidth: 1.0,
+                      const SizedBox(
+                        width: 20.0,
+                        height: 20.0,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              AppColors.lavenderHaze),
+                          strokeWidth: 1.0,
+                        ),
                       )
                     else
                       const Icon(
@@ -71,7 +96,7 @@ class PostInListWidget extends StatelessWidget {
               ],
             ),
           ),
-          const SizedBox(width: 5.0),
+          if (isAuthor) CustomPaint(painter: Triangle(Colors.grey[900]!)),
         ],
       ),
     );
