71016eb6a0870709b9f9cbf10735f2db0d01b79b
AzashiroMerume <kuandyksanzar020@gmail.com>
2023-11-07 20:46:23 +0600

    get pref

--- lib/api/user_preferences_api/user_preferences_api.dart
+++ lib/api/user_preferences_api/user_preferences_api.dart
@@ -43,3 +43,51 @@ Future<bool> savePreferences(List<String> preferences) async {
     throw NetworkException('Network error');
   }
 }
+
+Future<List<String>?> getPreferences() async {
+  final authToken = await storage.read(key: 'authToken');
+
+  if (authToken == null) {
+    throw TokenAuthException('Token authentication error');
+  }
+
+  try {
+    final response = await http.get(
+      Uri.parse('http://localhost:8081/preferences'),
+      headers: {
+        'Authorization': authToken,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // Parse the response JSON into a map
+      final Map<String, dynamic> responseData = json.decode(response.body);
+
+      if (responseData['success'] == true) {
+        final data = responseData['data'];
+
+        if (data != null) {
+          List<String> preferences = List<String>.from(data);
+          return preferences;
+        } else {
+          return null;
+        }
+      } else {
+        throw Exception('Preferences retrieval failed');
+      }
+    } else if (response.statusCode == 401) {
+      await storage.delete(key: 'authToken');
+      throw TokenAuthException('Token authentication error');
+    } else if (response.statusCode == 413) {
+      throw ContentTooLargeException('Content too large');
+    } else if (response.statusCode == 422) {
+      throw UnprocessableEntityException('Invalid input data');
+    } else if (response.statusCode >= 500) {
+      throw ServerException('Internal server error');
+    } else {
+      throw HttpException('Unexpected status code: ${response.statusCode}');
+    }
+  } on SocketException {
+    throw NetworkException('Network error');
+  }
+}
