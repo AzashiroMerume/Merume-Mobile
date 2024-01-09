16b359382346fee6759c8595b161d28f53aafa66
AzashiroMerume <kuandyksanzar020@gmail.com>
2024-01-09 16:52:07 +0600

    divided screen into two tabs

--- lib/screens/main/channel_screens/channel_details_screen.dart
+++ lib/screens/main/channel_screens/channel_details_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:merume_mobile/api/channel_api/channel_followers_api.dart';
 import 'package:merume_mobile/models/channel_model.dart';
@@ -19,189 +20,259 @@ class _ChannelDetailsScreenState extends State<ChannelDetailsScreen> {
   @override
   void initState() {
     super.initState();
-    _followersFuture = getChannelFollowers(widget.channel.id);
+    _followersFuture = getChannelFollowers(widget.channel.id); // Initial fetch
+    Timer.periodic(const Duration(seconds: 10), (Timer t) {
+      _followersFuture =
+          getChannelFollowers(widget.channel.id); // Fetch every 5 seconds
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''), // Remove the title from here
-        actions: [
-          IconButton(
-            onPressed: () {
-              print('hi');
-            },
-            icon: const Icon(
-              Icons.more_vert,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print('hi');
+              },
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+            ),
+          ],
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(20.0),
+            child: SizedBox(
+              height: 40.0, // Adjust the height as needed
+              child: TabBar(
+                indicatorColor: AppColors.lavenderHaze,
+                labelStyle: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.lavenderHaze,
+                ),
+                indicatorSize: TabBarIndicatorSize.tab,
+                tabs: [
+                  Tab(text: 'Details'),
+                  Tab(text: 'Followers'),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            detailsTab(),
+            followersTab(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget detailsTab() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
+            child: CircleAvatar(
+              radius: 80.0,
+              backgroundImage: widget.channel.channelProfilePictureUrl != null
+                  ? NetworkImage(widget.channel.channelProfilePictureUrl!)
+                  : const AssetImage('assets/images/isagi.jpg')
+                      as ImageProvider,
+            ),
+          ),
+          Text(
+            widget.channel.name,
+            style: const TextStyle(
+              fontSize: 24.0,
+              fontFamily: 'Poppins',
               color: Colors.white,
-            ), // Three dots icon
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: const Text(
+                    'Current Challenge Day',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Poppins',
+                      color: AppColors.mellowLemon,
+                    ),
+                  ),
+                  subtitle: Text(
+                    widget.channel.currentChallengeDay.toString(),
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'WorkSans',
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+                Divider(
+                  color: AppColors.lavenderHaze.withOpacity(0.5),
+                  thickness: 1,
+                ),
+                ListTile(
+                  title: const Text(
+                    'Subscribers',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Poppins',
+                      color: AppColors.mellowLemon,
+                    ),
+                  ),
+                  subtitle: Text(
+                    widget.channel.followers.currentFollowing.toString(),
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'WorkSans',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: AppColors.lavenderHaze.withOpacity(0.5),
+                  thickness: 1,
+                ),
+                ListTile(
+                  title: const Text(
+                    'Description',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Poppins',
+                      color: AppColors.mellowLemon,
+                    ),
+                  ),
+                  subtitle: Text(
+                    widget.channel.description,
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'WorkSans',
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+                Divider(
+                  color: AppColors.lavenderHaze.withOpacity(0.5),
+                  thickness: 1,
+                ),
+                ListTile(
+                  title: const Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Poppins',
+                      color: AppColors.mellowLemon,
+                    ),
+                  ),
+                  subtitle: Text(
+                    widget.channel.categories.join(', '),
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'WorkSans',
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+                Divider(
+                  color: AppColors.lavenderHaze.withOpacity(0.5),
+                  thickness: 1,
+                ),
+                const SizedBox(height: 10.0),
+              ],
+            ),
           ),
         ],
-        iconTheme: const IconThemeData(color: Colors.white), // Back arrow color
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+    );
+  }
+
+  Widget followersTab() {
+    // FutureBuilder for followers list
+    return FutureBuilder<List<User>>(
+      future: _followersFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: const TextStyle(color: AppColors.lightGrey),
+            ),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text(
+              'No followers available',
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        } else {
+          // Display the list of followers here
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: CircleAvatar(
-                  radius: 80.0,
-                  backgroundImage: widget.channel.channelProfilePictureUrl !=
-                          null
-                      ? NetworkImage(widget.channel.channelProfilePictureUrl!)
-                      : const AssetImage('assets/images/isagi.jpg')
-                          as ImageProvider,
-                ),
-              ),
-              Text(
-                widget.channel.name,
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      title: const Text(
-                        'Current Challenge Day',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontFamily: 'Poppins',
-                          color: AppColors.mellowLemon,
+              Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final follower = snapshot.data![index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30.0,
+                          backgroundImage: follower.pfpLink != null
+                              ? NetworkImage(follower.pfpLink!)
+                              : const AssetImage('assets/images/isagi.jpg')
+                                  as ImageProvider,
+                        ),
+                        title: Text(
+                          follower.username,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 15,
+                          ),
+                        ),
+                        subtitle: Text(
+                          '@${follower.nickname}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 12,
+                          ),
                         ),
                       ),
-                      subtitle: Text(
-                        widget.channel.currentChallengeDay.toString(),
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontFamily: 'WorkSans',
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                    Divider(
-                      color: AppColors.lavenderHaze.withOpacity(0.5),
-                      thickness: 1,
-                    ),
-                    ListTile(
-                      title: const Text(
-                        'Subscribers',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontFamily: 'Poppins',
-                          color: AppColors.mellowLemon,
-                        ),
-                      ),
-                      subtitle: Text(
-                        widget.channel.followers.currentFollowing.toString(),
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontFamily: 'WorkSans',
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    Divider(
-                      color: AppColors.lavenderHaze.withOpacity(0.5),
-                      thickness: 1,
-                    ),
-                    ListTile(
-                      title: const Text(
-                        'Description',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontFamily: 'Poppins',
-                          color: AppColors.mellowLemon,
-                        ),
-                      ),
-                      subtitle: Text(
-                        widget.channel.description,
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontFamily: 'WorkSans',
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                    Divider(
-                      color: AppColors.lavenderHaze.withOpacity(0.5),
-                      thickness: 1,
-                    ),
-                    ListTile(
-                      title: const Text(
-                        'Categories',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontFamily: 'Poppins',
-                          color: AppColors.mellowLemon,
-                        ),
-                      ),
-                      subtitle: Text(
-                        widget.channel.categories.join(', '),
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontFamily: 'WorkSans',
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                    Divider(
-                      color: AppColors.lavenderHaze.withOpacity(0.5),
-                      thickness: 1,
-                    ),
-                    FutureBuilder<List<User>>(
-                      future: _followersFuture,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const CircularProgressIndicator(); // Show a loading indicator while fetching data
-                        } else if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else if (!snapshot.hasData ||
-                            snapshot.data!.isEmpty) {
-                          return const Text('No followers available');
-                        } else {
-                          // Display the list of followers here
-                          return Column(
-                            children: [
-                              const Text('Followers:',
-                                  style: TextStyle(fontSize: 18.0)),
-                              ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: snapshot.data!.length,
-                                itemBuilder: (context, index) {
-                                  final follower = snapshot.data![index];
-                                  return ListTile(
-                                    title: Text(follower.nickname),
-                                    subtitle: Text(follower.email),
-                                  );
-                                },
-                              ),
-                            ],
-                          );
-                        }
-                      },
-                    ),
-                  ],
+                    );
+                  },
                 ),
               ),
             ],
-          ),
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
