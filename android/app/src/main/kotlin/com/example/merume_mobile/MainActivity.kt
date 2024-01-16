973dbefa80ed23e96884e49f28aada47d23835e0
AzashiroMerume <kuandyksanzar020@gmail.com>
2024-01-16 17:48:17 +0600

    fixed error with debug provider in emulator

--- android/app/src/main/kotlin/com/example/merume_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/merume_mobile/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.example.merume_mobile
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
+import com.google.firebase.appcheck.appCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun initDebug() {
+        // [START appcheck_initialize_debug]
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance(),
+        )
+        // [END appcheck_initialize_debug]
+    }
 }
