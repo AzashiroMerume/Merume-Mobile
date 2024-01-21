2dbfcc6049a3eee39b79730f94f01d98f632e442
AzashiroMerume <kuandyksanzar020@gmail.com>
2024-01-21 15:00:19 +0600

    basic design impr

--- lib/screens/main/other_user_screens/other_user_screen.dart
+++ lib/screens/main/other_user_screens/other_user_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:merume_mobile/models/author_model.dart';
+import 'package:merume_mobile/models/user_model.dart';
+import 'package:merume_mobile/other/colors.dart';
+import 'package:merume_mobile/screens/components/last_time_online.dart';
 
 class OtherUserScreen extends StatefulWidget {
-  final Author user;
+  final User user;
 
   const OtherUserScreen({super.key, required this.user});
 
@@ -14,23 +16,56 @@ class _OtherUserScreenState extends State<OtherUserScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      appBar: AppBar(
+        title: Text(
+          widget.user.nickname,
+          style: const TextStyle(
+              fontFamily: 'Poppins', color: Colors.white, fontSize: 18.0),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 20.0, right: 30.0, left: 30.0),
-          child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Align(
+            alignment: Alignment.center,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment:
-                  CrossAxisAlignment.center, // or CrossAxisAlignment.stretch
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const SizedBox(height: 20.0),
                 CircleAvatar(
                   radius: 80.0,
                   backgroundImage: widget.user.pfpLink != null
                       ? NetworkImage(widget.user.pfpLink!)
-                      : const AssetImage('assets/images/pfp_symbol.jpg')
+                      : const AssetImage('assets/images/pfp_outline.png')
                           as ImageProvider,
                 ),
-                // Add other widgets as needed
+                const SizedBox(height: 20.0),
+                Text(
+                  widget.user.username,
+                  style: const TextStyle(
+                      fontSize: 24.0,
+                      color: AppColors.mellowLemon,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
+                  '@${widget.user.nickname}',
+                  style: const TextStyle(
+                      fontSize: 18.0, color: AppColors.lightGrey),
+                ),
+                const SizedBox(height: 5.0),
+                if (widget.user.isOnline)
+                  const Text(
+                    'Online',
+                    style: TextStyle(fontSize: 16.0, color: Colors.green),
+                  )
+                else
+                  Text(
+                    formatLastSeen(widget.user.lastTimeOnline),
+                    style: const TextStyle(fontSize: 16.0, color: Colors.grey),
+                  ),
               ],
             ),
           ),
--- lib/screens/main/tab_bar_screens/account_tab_bar_screen/account_screen.dart
+++ lib/screens/main/tab_bar_screens/account_tab_bar_screen/account_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:merume_mobile/api/auth_api/firebase_auth_api.dart';
-import 'package:merume_mobile/api/auth_api/logout_api.dart';
+import 'package:merume_mobile/other/colors.dart';
+import 'package:merume_mobile/screens/components/last_time_online.dart';
 import 'package:merume_mobile/user_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -14,25 +14,80 @@ class AccountScreen extends StatefulWidget {
 class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
-    NavigatorState state = Navigator.of(context);
-    final userInfoProvider = Provider.of<UserProvider>(context, listen: false);
+    final user = Provider.of<UserProvider>(context, listen: true).userInfo;
+
+    if (user == null) {
+      return const Scaffold(
+        body: Center(
+          child: Text(
+            'Oops! Something went wrong.\nPlease try again later.',
+            style: TextStyle(fontSize: 18.0, color: Colors.red),
+          ),
+        ),
+      );
+    }
 
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              final loggedOut = await logout();
-
-              await logoutFromFirebase();
-
-              if (loggedOut) {
-                userInfoProvider.setUser(null);
-                // Navigate to the login screen
-                state.pushNamedAndRemoveUntil('/login', (route) => false);
-              }
-            },
-            child: const Text("Log out"),
+      appBar: AppBar(
+        title: Text(
+          user.nickname,
+          style: const TextStyle(
+              fontFamily: 'Poppins', color: Colors.white, fontSize: 20.0),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20.0),
+                CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: user.pfpLink != null
+                      ? NetworkImage(user.pfpLink!)
+                      : const AssetImage('assets/images/pfp_outline.png')
+                          as ImageProvider,
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  user.username,
+                  style: const TextStyle(
+                      fontSize: 24.0,
+                      color: AppColors.mellowLemon,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'WorkSans'),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
+                  '@${user.nickname}',
+                  style: const TextStyle(
+                      fontSize: 18.0,
+                      color: AppColors.lightGrey,
+                      fontFamily: 'Poppins'),
+                ),
+                const SizedBox(height: 5.0),
+                if (user.isOnline)
+                  const Text(
+                    'Online',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.green,
+                        fontFamily: 'Poppins'),
+                  )
+                else
+                  Text(
+                    formatLastSeen(user.lastTimeOnline),
+                    style: const TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey,
+                        fontFamily: 'Poppins'),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
