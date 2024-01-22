f797338d411fdd666b99f410b86e0d95f4a3c1f9
AzashiroMerume <kuandyksanzar020@gmail.com>
2024-01-22 16:53:25 +0600

    channel card widget for profile channels

---
+++ lib/screens/main/components/channel_card_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:merume_mobile/models/channel_model.dart';
+import 'package:merume_mobile/other/colors.dart';
+
+class ChannelCard extends StatelessWidget {
+  final Channel channel;
+
+  const ChannelCard({super.key, required this.channel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.postMain,
+      elevation: 3.0,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ListTile(
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+        leading: CircleAvatar(
+          radius: 30.0,
+          child: ClipOval(
+            child: channel.channelProfilePictureUrl != null
+                ? Image.network(
+                    channel.channelProfilePictureUrl!,
+                    height: 60.0,
+                    width: 60.0,
+                    fit: BoxFit.cover,
+                  )
+                : Image.asset(
+                    'assets/images/pfp_outline.png',
+                    height: 60.0,
+                    width: 60.0,
+                    fit: BoxFit.cover,
+                  ),
+          ),
+        ),
+        title: Text(
+          channel.name,
+          style: const TextStyle(
+              fontSize: 18.0,
+              fontFamily: 'Poppins',
+              color: AppColors.lavenderHaze),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              channel.description,
+              style: const TextStyle(
+                  fontSize: 16.0, fontFamily: 'WorkSans', color: Colors.white),
+            ),
+            // const SizedBox(
+            //   height: 5.0,
+            // ),
+            Text(
+              'Subscribers: ${channel.followers.currentFollowing.toString()}',
+              style: const TextStyle(
+                  fontSize: 14.0, fontFamily: 'WorkSans', color: Colors.white),
+            )
+          ],
+        ),
+        // Add more details or actions as needed
+        onTap: () {
+          // Handle tap on the channel card
+          // You can navigate to the channel details or perform other actions
+        },
+      ),
+    );
+  }
+}
